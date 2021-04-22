49e245f03550fffcf75181cb6d850156386cec44
vovaivanitskyi <vovaivanitskyi@gmail.com>
2021-04-22 18:53:12 +0300

    create 404 age, form on contact us, fix pagination

--- wp-content/themes/diplom/single.php
+++ wp-content/themes/diplom/single.php
@@ -1,24 +1,7 @@
 <?php get_header() ?>
-
-    <!-- Page Title
-    ================================================== -->
-    <div id="page-title">
-
-        <div class="row">
-
-            <div class="ten columns centered text-center">
-                <h1>post page<span>.</span></h1>
-
-                <p>Aenean condimentum, lacus sit amet luctus lobortis, dolores et quas molestias excepturi
-                    enim tellus ultrices elit, amet consequat enim elit noneas sit amet luctu. </p>
-            </div>
-
-        </div>
-
-    </div> <!-- Page Title End-->
-
     <!-- Content
-    ================================================== -->
+       ================================================== -->
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <div class="content-outer">
 
         <div id="page-content" class="row">
@@ -29,16 +12,15 @@
 
                     <div class="entry-header cf">
 
-                        <h1><a href="<?php the_permalink(); ?>" title=""><?php the_title()?></a></h1>
+                        <h1><?php the_title()?></h1>
 
                         <p class="post-meta">
 
                             <time class="date" datetime="2014-01-14T11:24"><?php the_time('F jS Y');?></time>
                             /
                             <span class="categories">
-                     <?php the_tags('', ' / ')  ?>
-                     </span>
-
+                             <?php the_tags('', ' / ')  ?>
+                             </span>
                         </p>
 
                     </div>
@@ -50,59 +32,25 @@
                     <div class="post-content">
 
                         <p><?php the_content(); ?></p>
+<!--                        <ul class="post-nav cf">-->
+<!--                            <li class="prev"><a rel="prev" href="#"><strong>Previous Article</strong> Duis Sed Odio Sit Amet Nibh Vulputate</a></li>-->
+<!--                            <li class="next"><a rel="next" href="#"><strong>Next Article</strong> Morbi Elit Consequat Ipsum</a></li>-->
+<!--                        </ul>-->
 
                     </div>
 
                 </article> <!-- post end -->
-
-
-            </div> <!-- Primary End-->
+            <?php endwhile; ?>
+            <?php endif; ?>
+            </div>
 
             <div id="secondary" class="four columns end">
 
                 <?php get_sidebar(); ?>
 
-            </div> <!-- Secondary End-->
+            </div> <!-- Comments End -->
 
         </div>
 
     </div> <!-- Content End-->
-
-    <!-- Tweets Section
-    ================================================== -->
-    <section id="tweets">
-
-        <div class="row">
-
-            <div class="tweeter-icon align-center">
-                <i class="fa fa-twitter"></i>
-            </div>
-
-            <ul id="twitter" class="align-center">
-                <li>
-               <span>
-               This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-               Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-               <a href="#">http://t.co/CGIrdxIlI3</a>
-               </span>
-                    <b><a href="#">2 Days Ago</a></b>
-                </li>
-                <!--
-                <li>
-                   <span>
-                   This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                   Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                   <a href="#">http://t.co/CGIrdxIlI3</a>
-                   </span>
-                   <b><a href="#">3 Days Ago</a></b>
-                </li>
-                -->
-            </ul>
-
-            <p class="align-center"><a href="#" class="button">Follow us</a></p>
-
-        </div>
-
-    </section> <!-- Tweets Section End-->
-
 <?php get_footer() ?>
